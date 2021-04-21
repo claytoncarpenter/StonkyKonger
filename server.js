2bb0f0b71588ab34774bbd457c45c75b741887e0
ozite <claytonwc672@gmail.com>
2021-04-20 21:48:02 -0500

    commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/stonky-konger'));
+app.use(express.static(__dirname + '/dist/StonkyKonger'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/stonky-konger/index.html'));});
+    '/dist/StonkyKonger/index.html'));});
 app.listen(process.env.PORT || 8080);
